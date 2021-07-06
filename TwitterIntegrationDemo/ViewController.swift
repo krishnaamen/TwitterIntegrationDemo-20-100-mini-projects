e45031b0c6df5954c6edbe2f7a7e17353d06f260
Krishna Prasad Khanal <krishnaamen@gmail.com>
2021-07-06 11:58:28 +0200

    first commit

--- TwitterIntegrationDemo/ViewController.swift
+++ TwitterIntegrationDemo/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var logInBtn: UIButton!
+    @IBOutlet weak var logOutBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        FHSTwitterEngine.shared()?.permanentlySetConsumerKey("Consumer Key from tda", andSecret: "Secretekey from your twitter developer account")
+        
+        
+        
     }
 
 
+    @IBAction func logInBtnPressed(_ sender: UIButton) {
+        let login = (FHSTwitterEngine.shared()?.loginController(completionHandler: { (sucess) in
+            self.logInBtn.isHidden = true
+            self.logOutBtn.isHidden = false
+        }))! as UIViewController
+        self.present(login, animated: true, completion: nil)
+    }
+    @IBAction func logOutBtnPressed(_ sender: UIButton) {
+        FHSTwitterEngine.shared()?.clearAccessToken()
+        self.logInBtn.isHidden = false
+        self.logOutBtn.isHidden = true
+    }
 }
 
